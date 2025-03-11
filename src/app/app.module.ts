696fc0d41a43b9cbc959fc1488d63f5955e2bf47
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-03-11 02:40:04 +0200

    finished the two tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TaskTwoComponent } from './task-two/task-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    FooterComponent,
+    TaskTwoComponent
   ],
   imports: [
     BrowserModule,
